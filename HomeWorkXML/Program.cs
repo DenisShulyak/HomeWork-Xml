890329d2d4035d85f67f97d4de668bc403f3e0de
DenisShulyak <denshufly@mail.ru>
2019-03-10 20:40:44 +0600

    HomeWork

---
+++ HomeWorkXML/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HomeWorkXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+           // Task2();
+        }
+
+
+        private static void Task1()
+        {
+            List<Item> list = new List<Item>();
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("XMLFile.xml");
+            var items = xmlDocument.DocumentElement.GetElementsByTagName("item");
+            foreach (XmlElement item in items)
+            {
+                list.Add(new Item()
+                {
+                    Title = item.ChildNodes.Item(0).InnerText,
+                    Guid = item.ChildNodes.Item(1).InnerText,
+                    Link = item.ChildNodes.Item(2).InnerText,
+                    Description = item.ChildNodes.Item(3).InnerText,
+                    PubDate = item.ChildNodes.Item(4).InnerText,
+                });
+            }
+            foreach (var item in list)
+            {
+                Console.Write("{0}\n{1}\n{2}\n{3}\n{4}\n\n", item.Title, item.Guid, item.Link, item.Description, item.PubDate);
+            }
+            Console.ReadLine();
+        }
+        private static void Task2()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            var students = xmlDocument.CreateElement("students");
+            xmlDocument.AppendChild(students);
+            var student = xmlDocument.CreateElement("student");
+            student.SetAttribute("FirstName", "Александр");
+            student.SetAttribute("LastName", "Васильев");
+            student.SetAttribute("Gender", "Male");
+            students.AppendChild(student);
+            Console.WriteLine(xmlDocument.DocumentElement.FirstChild.Attributes["FirstName"].Value + " " + xmlDocument.DocumentElement.FirstChild.Attributes["LastName"].Value + " " + xmlDocument.DocumentElement.FirstChild.Attributes["Gender"].Value);
+            Console.ReadLine();
+        }
+    }
+}
